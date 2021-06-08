16a84996271eec28f366de9018f444d02b5cf2e2
Odeh <abuzaid163@hotmail.com>
2021-06-08 09:56:47 +0300

    correcting assignment requirement submition

--- js/app.js
+++ js/app.js
@@ -5,28 +5,50 @@ let visitorName = prompt('Hi , Enter your name please') ;
 alert(`Hello ${visitorName} welcome to my humble page ,
 i will ask you a quick five questions about me. Guess ( y or n / yes or no) !`);
 
+
 //five questions and if answered with correctly with  yes/no | y/n  alert and (log to console in a comment)
-let answer = prompt('i am from jordan !').toLowerCase();
+let answer;
+do
+{
+  answer = prompt('i am from jordan !').toLowerCase();
+}while ( (answer !== 'yes' ) && (answer !== 'y') && (answer !== 'no') && (answer !== 'n') );
 if ( answer === 'yes' || answer === 'y'){
   alert('thats corret i am from jordan');
   /*console.log(`visitor ${visitorName} Answerd where i am from correctly `); */
 }
-answer = prompt('i like cofee more than tea !').toLowerCase();
+
+do
+{
+  answer = prompt('i like cofee more than tea !').toLowerCase();
+}while ( (answer !== 'yes' ) && (answer !== 'y') && (answer !== 'no') && (answer !== 'n') );
 if ( answer === 'yes' || answer === 'y'){
   alert('thats corret i like cofee more than tea');
   /*console.log(`visitor ${visitorName} Answerd what is my favorite drink correctly `); */
 }
-answer = prompt('i am an organized person').toLowerCase();
+
+do
+{
+  answer = prompt('i am an organized person').toLowerCase();
+}while ( (answer !== 'yes' ) && (answer !== 'y') && (answer !== 'no') && (answer !== 'n') );
+
 if ( answer === 'yes' || answer === 'y'){
   alert('thats corret i am organized person');
   /*console.log(`visitor ${visitorName} Answerd if i am organized person or not correctly `); */
 }
-answer = prompt('do i like basket ball !').toLowerCase();
+
+do
+{
+  answer = prompt('do i like basket ball !').toLowerCase();
+}while ( (answer !== 'yes' ) && (answer !== 'y') && (answer !== 'no') && (answer !== 'n') );
 if ( answer === 'no' || answer === 'n'){
   alert('thats right');
   /*console.log(`visitor ${visitorName} Answerd if i like basketball or not correctly `); */
 }
-answer = prompt('do i like reading!').toLowerCase();
+
+do
+{
+  answer = prompt('do i like reading!').toLowerCase();
+}while ( (answer !== 'yes' ) && (answer !== 'y') && (answer !== 'no') && (answer !== 'n') );
 if ( answer === 'no' || answer === 'n'){
   alert('thats right, i cant read for more than 15 minuetes without a 15 minutes break :D ');
   /*console.log(`visitor ${visitorName} Answerd if i like reading or not correctly `); */
